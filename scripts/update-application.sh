399f03c56f9deea9ba3e0616effd2b9ed499134e
Vo The Minh <18120211>
2022-05-31 18:51:05 +0700

    up

--- scripts/update-application.sh
+++ scripts/update-application.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-aws eks --region us-east-2 --name udacity update-kubeconfig 
+# gcloud container clusters get-credentials mycluster --zone us-central1-c --project udacity-351902
+kubectl config use-context  
+echo "----------------GET NODES------------------"
+kubectl get node
+echo "----------------GET PODS-------------------"
+kubectl get pod
+echo "----------------GET DEPLOY-----------------"
+kubectl apply -f nginx/deploy-udacity-nginx.yml
+kubectl rollout restart deploy udacity-nginx
+kubectl get deploy
+echo "----------------GET SERVICE----------------"
+kubectl expose deploy udacity-nginx --port=80 --type=LoadBalancer
+kubectl get svc
+
